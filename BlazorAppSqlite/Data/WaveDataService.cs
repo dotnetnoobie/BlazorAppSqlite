9b6f5ba735e2f21e54666ae6b0f5917f5253efaa
Administrator <administrator@ngandugabrieloutlook.onmicrosoft.com>
2021-12-20 19:19:21 +0100

    Added persistent DB with IndexedDB backend

---
+++ BlazorAppSqlite/Data/WaveDataService.cs
@@ -0,0 +1,16 @@
+﻿using BlazorAppSqlite.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppSqlite.Data
+{
+    public static class WaveDataService
+    {
+
+        public static void AddWaveDataDbContext(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContextFactory<ClientSideDbContext>(
+                options => options.UseSqlite($"Filename={DataSynchronizer.SqliteDbFilename}"));
+            serviceCollection.AddScoped<DataSynchronizer>();
+        }
+    }
+}
